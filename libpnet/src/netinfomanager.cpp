aea5db798095ffff533929c72315b2dd38b7edec
Pol4bear <root@pol4.dev>
2024-05-19 10:59:20 +0900

    최적 라우팅 정보 조회시 의미없는 조건문 제거

--- libpnet/src/netinfomanager.cpp
+++ libpnet/src/netinfomanager.cpp
@@ -187,7 +187,7 @@ const RouteInfo *NetInfoManager::get_best_routeinfo(IPv4Addr destination) {
     if ((destination & route.mask) == route.destination) {
       int prefix_len = SubnetMask(route.mask).to_cidr();
       if (prefix_len > longest_prefix || (prefix_len == longest_prefix &&
-        (route.metric < best_route->metric || (route.metric == best_route->metric && best_route->gateway == 0)))) {
+        route.metric < best_route->metric)) {
         longest_prefix = prefix_len;
         best_route = &route;
       }
